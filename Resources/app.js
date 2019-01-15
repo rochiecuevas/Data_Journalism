b454a39e5719d3f5f5f7968983aa6c72c2dbb24b
Rochie Cuevas <rochiecuevas@gmail.com>
2019-01-14 22:05:59 -0800

    graph poverty vs obesity created

--- Resources/app.js
+++ Resources/app.js
@@ -1,11 +1,11 @@
 // Define the SVG object that will contain the chart in the HTML
-var svgHeight = 960;
-var svgWidth = 500;
+var svgHeight = 500;
+var svgWidth = 1000;
 
 var margins = {
     top: 40,
     right: 40,
-    bottom: 40,
+    bottom: 60,
     left: 40
 };
 
@@ -20,224 +20,91 @@ var svg = d3.select("#svg-spot") // "svg-spot" is a div id hence the pound sign
 
 // Add the chart to the SVG object
 var chartGroup = svg.append("g")
-                    .attr("transform", `translate(${margins.left}, ${margins.top})`);
+                    .attr("transform", `translate(${margins.left + 100}, ${margins.top})`);
 
-// Prepare the dynamic charts
-
-// 1. Default chosen x-axis
-var chosenXAxis = "poverty";
-console.log(chosenXAxis);
-
-// 2. Define a function that updates the x-axis upon clicking a label
-function xScale(data, chosenXAxis){
-
-    // create scales
-    var xLinearScale = d3.scaleLinear()
-                         .domain([d3.min(data, d => d[chosenXAxis]), d3.max(data, d => d[chosenXAxis])])
-                         .range([0, width]);
-
-    return xLinearScale;
-    
-};
-
-// 3. Define a function that updates the x-axis when an x-axis label is chosen
-function renderAxis(newXAxis, xAxis){
-    var bottomAxis = d3.bottomAxis(newXAxis);
-
-    xAxis.transition()
-         .duration(1000) // duration of transition is 1 second
-         .call(bottomAxis);
-
-    return xAxis;     
-};
-
-// 4. Define a function that updates circles in the chart during transition
-function renderCircles(circlesGroup, newXAxis, chosenXAxis){
-
-    circlesGroup.transition()
-                .duration(1000) // duration of transition is 1 second
-                .attr("cx", d => newXAxis(d[chosenXAxis]));
-
-    return circlesGroup;
-};
-
-// // 5. Define a function that updates the tooltip to each circle
-function updateToolTip(chosenXAxis, circlesGroup){
-    if (chosenXAxis === "poverty"){
-        var label = "Below Poverty Line (%):"
-    } else if (chosenXAxis === "age"){
-        var label = "Median Age (yrs):"
-    } else if (chosenXAxis === "income"){
-        var label = "Median Household Income (USD):"
-    };
-
-    var toolTip = d3.select("body")
-                    .append("div")
-                    .attr("class", "tooltip");
-
-    // Show the tooltip when the mouse is over the circle
-    circlesGroup.on("mouseover", function(data, i){
-        toolTip.style("display", "block");
-        toolTip.html(`${data.state[i]}<hr>${label} ${data.poverty[i]}`)
-               .style("left", d3.event.pageX + "px")
-               .style("top", d3.event.pageY + "px");
-    })
-
-    // Tooltip is hidden once the mouse is moved
-            .on("mouseout", function(){
-                toolTip.style("display", "none");
-            });
-    
-    return circlesGroup;
-};
-
-
-// 6. Import data
+// Load data from data.csv
 d3.csv("Resources/data.csv", function(error, data){
+    // If there's an error, throw an error
     if (error) throw error;
+    
+    // Otherwise, print the data
     console.log(data);
 
-    // 6a. Convert numerical data from string to float
-    data.forEach(function(data){
-        data.poverty = +data.poverty;
-        data.povertyMoe = +data.povertyMoe;
-        data.age = +data.age;
-        data.ageMoe = +data.ageMoe;
-        data.income = +data.income;
-        data.incomeMoe = +data.incomeMoe;
-        data.noHealthInsurance = +data.noHealthInsurance;
-        data.obesity = +data.obesity;
-        data.smokes = +data.smokes;
+    // Convert the numbers from strings to floats
+    data.forEach(function(d){
+        d.poverty = +d.poverty;
+        d.age = +d.age;
+        d.income = +d.income;
+        d.obesity = +d.obesity;
+        d.smokes = +d.smokes;
+        d.noHealthInsurance = +d.noHealthInsurance;
     });
 
-    // 6b. Use the xScale function to update the x-axis depending on the chosen x-axis label
-    var xLinearScale = xScale(data, chosenXAxis);
+    // Configure poverty as the x-axis
+    var xPovertyScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.poverty))
+        .range([0, width]);
 
-    // 6c. Create a y-scale function
-    var yLinearScale = d3.scaleLinear()
-                         .domain([d3.min(data, d => d.noHealthInsurance), d3.max(data, d => d.noHealthInsurance)])
-                         .range(height, 0);
-
-    // 6d. Set the axis functions
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
-
-    // 6e. Append an x-axis
-    var xAxis = chartGroup.append("g")
-                          .classed("x-axis", true)
-                          .attr("transform", `translate(0, ${height})`)
-                          .call(bottomAxis);
-                
-    // 6f. Append a y-axis
-    chartGroup.append("g")
-              .call(leftAxis);
+    // Configure obesity as the y-axis
+    var yObesityScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.obesity))
+        .range([height, 0]);
     
-    // 6g. Append initial circles
+    // Create new functions for the axes
+    var bottomAxis = d3.axisBottom(xPovertyScale);
+    var leftAxis = d3.axisLeft(yObesityScale);
+
+    // Append an SVG group element for the left y-axis to the chartGroup
+    chartGroup.append("g")
+              .classed("axis", true)
+              .call(leftAxis);
+
+    // Append an SVG group element for the bottom x-axis to the chartGroup
+    chartGroup.append("g")
+              .classed("axis", true)
+              .attr("transform", `translate(0,${height})`)
+              .call(bottomAxis);
+
+    // Create circles that will correspond to the data points (scatter plot)
     var circlesGroup = chartGroup.selectAll("circle")
                                  .data(data)
                                  .enter()
                                  .append("circle")
-                                 .attr("cx", d => xLinearScale(d[chosenXAxis]))
-                                 .attr("cy", d => yLinearScale(d.noHealthInsurance))
-                                 .attr("r", 20)
+                                 .attr("cx", d => xPovertyScale(d.poverty))
+                                 .attr("cy", d => yObesityScale(d.obesity))
                                  .attr("fill", "green")
-                                 .attr("opacity", 0.75);
+                                 .attr("r", "10")
+                                 .attr("opacity", "0.75");
 
-    // 6h. Create a group containing three x-axis labels
-    var xlabelsGroup = chartGroup.append("g")
-                                 .attr("transform", `translate(${width / 2}, ${height + 20})`);
+    // Create tooltips
+    var toolTips = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d){
+            return(`<strong>${d.state}</strong><br> Obesity (%): ${d.obesity} <br> Poverty(%): ${d.poverty}`);
+        });
+    
+    chartGroup.call(toolTips);
 
-    var povertyLabel = xlabelsGroup.append("text")
-                                   .attr("x", 0)
-                                   .attr("y", 20)
-                                   .attr("value", "poverty")
-                                   .classed("active", true)
-                                   .text("Below Poverty Level (%)");
+    // Create event listeners so that when a point is clicked, the tooltip is shown
+    circlesGroup.on("mouseover", function(data){
+        toolTips.show(data, this);
+    })
+        .on("mouseout", function(data){
+            toolTips.hide(data);
+        });
 
-    var ageLabel = xlabelsGroup.append("text")
-                                   .attr("x", 0)
-                                   .attr("y", 20)
-                                   .attr("value", "age")
-                                   .classed("active", true)
-                                   .text("Median Age (yrs)");
-
-    var incomeLabel = xlabelsGroup.append("text")
-                                  .attr("x", 0)
-                                  .attr("y", 20)
-                                  .attr("value", "income")
-                                  .classed("active", true)
-                                  .text("Median Household Income (USD)");
-
-    // 6i. Append the y-axis
+    // Create axis labels
     chartGroup.append("text")
-              .attr("transform", "rotate(-90)")
-              .attr("y", 0 - margins.left)  
-              .attr("x", 0 - (height / 2))
-              .attr("dy", "1em")
-              .classed("axis-text", true)
-              .text("No Health Insurance (%)");
-        
-    // 6j. Use updateToolTip to update circles
-    var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - (margins.left + 25))
+        .attr("x", 0 - (height - 60))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Proportion of Obese People in the Population (%)");
 
-    // 6k. Add an event listener so that something happens when an x-axis label is chosen
-    xlabelsGroup.selectAll("text")
-                .on("click", function(){
-
-                    // Get the value of the selection (x-axis label)
-                    var xSelection = d3.select(this).attr("value");
-
-                    if (value !== chosenXAxis){
-
-                        // Replace chosenXAxis with value (x-axis label)
-                        chosenXAxis == xSelection;
-                        console.log(chosenXAxis);
-
-                        // Update the x-axis scale for the new x-axis values
-                        xLinearScale = xScale(data, chosenXAxis);
-
-                        // Update x-axis with transition
-                        xAxis = renderAxis(xLinearScale, xAxis);
-
-                        // Update circles based on new x-axis values
-                        circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
-
-                        // Update tooltips based on chosen x-axis label
-                        circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-
-                        // Make the selected x-axis bold font
-                        if (chosenXAxis === "poverty"){
-                            povertyLabel
-                                .classed("active", true)
-                                .classed("inactive", false);
-                            ageLabel
-                                .classed("active", false)
-                                .classed("inactive", true);
-                            incomeLabel
-                                .classed("active", false)
-                                .classed("inactive", true);
-                        } else if (chosenXAxis === "age"){
-                            povertyLabel
-                                .classed("active", false)
-                                .classed("inactive", true);
-                            ageLabel
-                                .classed("active", true)
-                                .classed("inactive", false);
-                            incomeLabel
-                                .classed("active", false)
-                                .classed("inactive", true);
-                        } else if (chosenXAxis === "income"){
-                            povertyLabel
-                                .classed("active", false)
-                                .classed("inactive", true);
-                            ageLabel
-                                .classed("active", false)
-                                .classed("inactive", true);
-                            incomeLabel
-                                .classed("active", true)
-                                .classed("inactive", false);
-                        }
-                    }
-
-                });
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 3}, ${height + margins.top})`)
+        .attr("class", "axisText")
+        .text("Proportion of People in Poverty (%)");    
 });
